e932783b53f5b46e8d2f0ed8ee6599f14195d870
Marshall <marshallycheng@gmail.com>
2018-05-08 20:26:09 -0700

    Attempt User Auth

---
+++ w4d2/app_name/app/models/user.rb
@@ -0,0 +1,27 @@
+class User < ApplicationRecord
+  before_validation :ensure_session_token
+  validates :username, :session_token, presence: true
+  validates :password, length: {minimum: 6, allow_nil: true}, presence: {message:'Password cant be blank'}
+  validates :password_digest, presence: true
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+    nil
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+end
